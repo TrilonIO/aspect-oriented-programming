c3567ed2c2c5a0693d6668b0e2bed6881cfa7cdd
Maciej Sikorski <maciejsikorski00@gmail.com>
2022-09-10 12:42:34 +0200

    log around

--- src/adoption/adoption.service.ts
+++ src/adoption/adoption.service.ts
@@ -3,15 +3,63 @@ import { EventPublisher } from '@nestjs/cqrs';
 import { RequestId } from 'src/common/request-id';
 import { Connection } from 'src/database/connection';
 import { Datadog } from 'src/datadog/datadog';
+import { LogAroundDecorator } from 'src/logging/logging.decorator';
 import { Metrics } from 'src/metrics/metrics';
 import { AdoptionRequestFactory } from './adoption-request.factory';
 import { AdoptionRepository } from './adoption.repository';
 import { DataNeededForAdoption } from './data-needed-for-adoption.dto';
 
+// @Injectable()
+// export class AdoptService {
+//   private readonly logger = new Logger('Adopt');
+
+//   constructor(
+//     private readonly adoptionRepository: AdoptionRepository,
+//     private readonly adoptionRequestFactory: AdoptionRequestFactory,
+//     private readonly connection: Connection,
+//     private eventPublisher: EventPublisher,
+//     private readonly datadog: Datadog,
+//     private readonly metrics: Metrics,
+//   ) {}
+
+//   async adopt(dto: DataNeededForAdoption, requestId: RequestId): Promise<void> {
+//     try {
+//       this.logger.log({
+//         message: 'Adoption started',
+//         data: { ...dto, requestId },
+//       });
+//       this.metrics.begin(requestId);
+
+//       await this.connection.transaction(async (transaction) => {
+//         const adoptionRequest = await this.adoptionRequestFactory.create(
+//           dto,
+//           transaction,
+//         );
+//         const adoption = adoptionRequest.adopt();
+//         this.eventPublisher.mergeObjectContext(adoption);
+//         await this.adoptionRepository.create(adoption, transaction);
+//         adoption.commit();
+//       });
+
+//       this.metrics.finish(requestId);
+//       this.logger.log({
+//         message: 'Adoption completed',
+//         data: { ...dto, requestId },
+//       });
+//     } catch (e) {
+//       this.logger.error({
+//         message: 'Adoption failed',
+//         error: e,
+//         data: { ...dto, requestId },
+//       });
+//       this.datadog.registerError(e);
+//       throw e;
+//     }
+//   }
+// }
+
 @Injectable()
 export class AdoptService {
-  private readonly logger = new Logger('Adopt');
-
   constructor(
     private readonly adoptionRepository: AdoptionRepository,
     private readonly adoptionRequestFactory: AdoptionRequestFactory,
@@ -21,15 +69,12 @@ export class AdoptService {
     private readonly metrics: Metrics,
   ) {}
 
+  @LogAroundDecorator('Adoption')
   async adopt(dto: DataNeededForAdoption, requestId: RequestId): Promise<void> {
     try {
-      this.logger.log({
-        message: 'Adoption started',
-        data: { ...dto, requestId },
-      });
       this.metrics.begin(requestId);
 
-      await this.connection.transaction(async (transaction) => {
+      this.connection.transaction(async (transaction) => {
         const adoptionRequest = await this.adoptionRequestFactory.create(
           dto,
           transaction,
@@ -41,52 +86,12 @@ export class AdoptService {
       });
 
       this.metrics.finish(requestId);
-      this.logger.log({
-        message: 'Adoption completed',
-        data: { ...dto, requestId },
-      });
     } catch (e) {
-      this.logger.error({
-        message: 'Adoption failed',
-        error: e,
-        data: { ...dto, requestId },
-      });
       this.datadog.registerError(e);
       throw e;
     }
   }
 }
-
-/**
-@Injectable()
-export class AdoptService {
-   ... Dependency Injection
-
-   @LogAroundDecorator('Adoption')
-   adopt(dto: DataNeededForAdoption, requestId: RequestId): Promise<void> {
-     try {
-       this.metrics.begin(requestId);
- 
-       this.connection.transaction((transaction) => {
-         const adoptionRequest = await this.adoptionRequestFactory.create(
-           dto,
-           transaction
-         );
-         const adoption = adoptionRequest.adopt();
-         this.eventPublisher.mergeObjectContext(adoption);
-         await this.adoptionRepository.create(adoption, transaction);
-         adoption.commit();
-       });
- 
-       this.metrics.finish(requestId);
-     } catch (e) {
-       this.dataDog.registerError(e);
-       throw e;
-     }
-   }
- }
- 
- */
 
 /**
 @Injectable()
